6fdd85232daed5ca71617519af2e322b91244a9e
ChrisCox12 <chriscox9856@gmail.com>
2022-06-29 22:34:14 -0400

    fixes to axios util

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,24 +2,17 @@ import axios from 'axios';
 
 
 const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
-let headers = {};
-
-
-if(localStorage.getItem('invoice-app-user')) {
-    //  sets authorization header for token authentication
-    headers.authorization = `Bearer ${localStorage.getItem('invoice-app-user')}`;
-}
 
 //  creates an axios instance that can used across the application
-const axiosInstance = axios.create({
-    baseURL: REACT_APP_API_URL,
-    headers
-});
+const axiosInstance = axios.create({ baseURL: REACT_APP_API_URL });
 
 //  by using interceptors, ensures that authorization header is set before every request
 axiosInstance.interceptors.request.use(
     function(config) {
-        config.headers['authorization'] = `Bearer ${localStorage.getItem('invoice-app-user')}`;
+        if(localStorage.getItem('invoice-app-user')) {
+            config.headers['authorization'] = `Bearer ${localStorage.getItem('invoice-app-user')}`;
+        }
+        
         return config;
     }, 
     function(error) {
